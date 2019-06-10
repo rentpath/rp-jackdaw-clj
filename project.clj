f97a82dae8775d351a4b499c09d6d411eb42fe2c
Jenkins Service <jenkins@jenkins-slave003.tools.useast2.rentpath.com>
2019-06-10 14:06:20 -0400

    Version 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/rp-jackdaw-clj "0.0.1"
+(defproject com.rentpath/rp-jackdaw-clj "0.0.2-SNAPSHOT"
   :description "Clojure Kafka components using Jackdaw"
   :url "https://gitthub.com/rentpath/rp-jackdaw-clj"
   :license {:name "MIT"
