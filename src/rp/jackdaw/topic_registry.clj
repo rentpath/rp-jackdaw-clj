4bfa3b713f0dc48c2bd67eccec2e71c6d8185f40
Jeremy Holland <jeremy@jeremypholland.com>
2020-03-07 10:12:57 -0600

    Adding schema retrieval from schema registry to topic registry

--- src/rp/jackdaw/topic_registry.clj
+++ src/rp/jackdaw/topic_registry.clj
@@ -1,6 +1,7 @@
 (ns rp.jackdaw.topic-registry
   (:require [com.stuartsierra.component :as component]
-            [rp.jackdaw.resolver :as resolver]))
+            [rp.jackdaw.resolver :as resolver]
+            [rp.jackdaw.schema :as schema]))
 
 ;; `topic-metadata` is a map of "unresolved" topic metadata.
 (defrecord BaseTopicRegistry [topic-metadata serde-resolver-fn]
@@ -12,6 +13,24 @@
   (stop [this]
     this))
 
+(defn resolve-schema
+  [{:keys [schema-name schema-version]} schema-registry-url]
+  (schema/fetch-schema schema-registry-url schema-name schema-version))
+
+
+(defn resolve-schemata
+  [topic-metadata schema-registry-url]
+  (into {}
+        (map (fn [topic-kw {:keys [topic-name key-serde value-serde] :as topic-config}]
+               (let [{key-serde-keyword :serde-keyword} key-serde
+                     {value-serde-keyword :serde-keyword} value-serde]
+                 [topic-kw (-> topic-config
+                               (= key-serde-keyword :jackdaw.serdes.avro.confluent/serde)
+                               (assoc-in [:key-serde :schema] (resolve-schema key-serde schema-registry-url))
+                               (= value-serde-keyword :jackdaw.serdes.avro.confluent/serde)
+                               (assoc-in [:key-serde :schema] (resolve-schema value-serde schema-registry-url)))])))
+        topic-metadata))
+
 ;;
 ;; Note that `type-registry` may be `nil` or left unspecified.
 ;; In such a case jackdaw's default type registry will be used.
@@ -19,7 +38,7 @@
 
 (defn map->TopicRegistry
   [{:keys [topic-metadata schema-registry-url type-registry]}]
-  (map->BaseTopicRegistry {:topic-metadata topic-metadata
+  (map->BaseTopicRegistry {:topic-metadata    (resolve-schemata topic-metadata schema-registry-url)
                            :serde-resolver-fn (resolver/serde-resolver schema-registry-url type-registry)}))
 
 (defn map->MockTopicRegistry
